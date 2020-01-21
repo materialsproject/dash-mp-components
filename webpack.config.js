0143c18a65f81a4161dec6a5dcecdc5913e66d20
Francois Chabbey <francois@streamsets.com>
2020-01-20 18:57:31 -0800

    Update package.json. Setup webpack
    - We want to be able to bundle/load css and images
    - We want to use the periodic table component

--- webpack.config.js
+++ webpack.config.js
@@ -75,6 +75,10 @@ module.exports = (env, argv) => {
                         },
                     ],
                 },
+                {
+                    test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                    loader: 'url-loader?limit=100000'
+                }
             ],
         },
     }
