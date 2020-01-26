495e623f65dcdf79eb5fa0e9de92914d35c9082d
Francois Chabbey <francois@streamsets.com>
2020-01-26 11:42:15 -0800

    Extract test page classes in their own file

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,6 +1,7 @@
 import dash_mp_components
 import dash
 from dash.dependencies import Input, Output
+from .periodic_table import PeriodicTable
 import dash_html_components as html
 from dash.testing.application_runners import import_app
 import time
@@ -13,31 +14,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 # TODO(chab)
 # ST : refine and tests API
 # LT : break into multiple small tests, build a page API for each component
-
-
-
-class BasePage(object):
-    """Base class to initialize the base page that will be called from all pages"""
-
-    def __init__(self, dash_duo):
-        self.dash_duo = dash_duo;
-
-class PeriodicTable(BasePage):
-
-    def findElement(self, symbol):
-        selector = "//div[./*[normalize-space() = '" + symbol + "']]";
-        return self.dash_duo.driver.find_element_by_xpath(selector)
-    def checkDetailed(self, number, symbol, name):
-        detailed_element = self.dash_duo.driver.find_element_by_css_selector(".detailed")
-        detailed_element.find_element_by_css_selector(".number").text == number
-        detailed_element.find_element_by_css_selector(".symbol").text == symbol
-        detailed_element.find_element_by_css_selector(".name").text == name
-    def check_if_element_has_class(self, element_symbol, klass):
-        assert (klass in self.findElement(element_symbol).get_attribute('class').split()) == True
-    def hover_over_element(self, symbol):
-        element_to_hover_over = self.findElement(symbol)
-        hover = ActionChains(self.dash_duo.driver).move_to_element(element_to_hover_over)
-        hover.perform()
 
 def test_render_component(dash_duo, mocker):
     # Start a dash app contained as the variable `app` in `usage.py`
