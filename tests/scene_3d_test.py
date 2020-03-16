81d37fd9eae35bcc281d45b83a8323261f6120f2
Francois Chabbey <francois@streamsets.com>
2020-03-16 10:51:58 +0100

    Handles scene changes. Add test

--- tests/scene_3d_test.py
+++ tests/scene_3d_test.py
@@ -1,11 +1,15 @@
 import dash_mp_components
 import dash
 import dash_html_components as html
+import dash_core_components as dcc
 from dash_mp_components.test_api import resize_browser_window
+from dash.dependencies import Input, Output
+import time
 
 import pytest
 import unittest
-from .scene import scene
+from .scene import scene, scene2, scene3
+from selenium.webdriver.common.keys import Keys
 
 
 def check_path(dash_duo, number_of_path):
@@ -26,15 +30,33 @@ class SVG3DScene(unittest.TestCase):
         self.app = dash.Dash(__name__)
         resize_browser_window(1920, 1080, self.dash_duo.driver)
         self.app.layout = html.Div(
-            style={'width':'500px', 'height':'500px'},
-            children=[
-                dash_mp_components.Simple3DScene(
-                    id='3d',
-                    settings={"renderer": 'svg'},
-                    data=scene
-                )
-            ]
-        )
+                children=[
+                    dcc.Dropdown(
+                        id='demo-dropdown',
+                        options=[
+                            {'label': 'Scene1', 'value': scene},
+                            {'label': 'Scene2', 'value': scene2},
+                            {'label': 'Scene3', 'value': 'cc'}
+                        ],
+                        value=scene
+                    ),
+                    html.Div(
+                        style={'width': '500px', 'height': '500px'},
+                        children=[
+                            dash_mp_components.Simple3DScene(
+                                id='3d',
+                                settings={"renderer": 'svg'},
+                                data=scene
+                            )
+                        ]
+                    )
+                ]
+            )
+
+        @self.app.callback(Output('3d', 'data'), [Input('demo-dropdown', 'value')])
+        def display_output(value):
+            print("chosen value", value)
+            return value
         self.dash_duo.start_server(self.app)
         # wait for table to be there
         print('Test started', __name__)
@@ -54,3 +76,17 @@ class SVG3DScene(unittest.TestCase):
         check_path_color(self.dash_duo.find_elements('path')[4], 'rgb(255, 170, 170)')
         check_path_color(self.dash_duo.find_elements('path')[5], 'rgb(17, 17, 17)')
         check_path_color(self.dash_duo.find_elements('path')[6], 'rgb(255, 170, 170)')
+    def test_scene_switcher(self):
+        # check if adding a new scene clean the old one
+        dropdown = self.dash_duo.find_element('#demo-dropdown input')
+        dropdown.send_keys('Scene2')
+        dropdown.send_keys(Keys.ENTER)
+        time.sleep(1)
+        # but there are 2 visible spheres, try to understand the path :/
+        check_path(self.dash_duo, 1)
+        dropdown.send_keys('Scene1')
+        dropdown.send_keys(Keys.ENTER)
+        time.sleep(1)
+        check_path(self.dash_duo, 7)
+
+
