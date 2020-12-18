57c01152d2bea8ee88fee98b1d24972b0d6ad9b9
Cody O'Donnell <ctodonnell@lbl.gov>
2020-12-18 12:18:22 -0500

    Fixes syntax error thrown by dash_mp_components.min.js.map

--- dash_mp_components/__init__.py
+++ dash_mp_components/__init__.py
@@ -44,7 +44,8 @@ _js_dist = [
         'relative_package_path': 'dash_mp_components.min.js.map',
         'external_url': 'https://unpkg.com/{0}@{2}/{1}/{1}.min.js.map'.format(
             package_name, __name__, __version__),
-        'namespace': package_name
+        'namespace': package_name,
+        'dynamic': True
     }
 ]
 
