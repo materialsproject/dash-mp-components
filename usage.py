02686b02d3373b46694dd583631bb4e7611613fe
Cody O'Donnell <ctodonnell@lbl.gov>
2022-01-20 19:09:40 -0800

    Upgrade mp-react-components

--- usage.py
+++ usage.py
@@ -1,5 +1,6 @@
 import dash_mp_components
 import dash
+from dash.dependencies import Input, Output, State
 from dash import dcc, html
 from tests.scene import scene
 import os
@@ -16,8 +17,17 @@ app.layout = html.Div(children=[
     settings={'renderer': 'svg', 'defaultZoom': 0.8},
     data=scene,
     sceneSize=500
-  )
+  ),
+  html.Div(id='image-output')
 ])
+
+@app.callback(
+    Output('image-output', 'children'),
+    Input('scene', 'imageDataTimestamp')
+)
+def get_image_data(image_data_timestamp):
+    print(image_data_timestamp)
+    return image_data_timestamp
 
 # use True to load a dev build of react
 if __name__ == '__main__':
