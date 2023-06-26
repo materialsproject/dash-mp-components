f8dc7ed44107f35769465051bd4e14c557ae3402
yang-ruoxi <roxie@Live.cn>
2023-06-26 12:31:38 -0700

    add propTypes

---
+++ dash_mp_components/NotificationDropdown.py
@@ -0,0 +1,45 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class NotificationDropdown(Component):
+    """A NotificationDropdown component.
+
+
+Keyword arguments:
+
+- children (a list of or a singular dash component, string or number; optional)
+
+- id (string; optional)
+
+- className (string; optional)
+
+- hasUnread (boolean; optional)
+
+- isHidden (boolean; optional)
+
+- isModal (boolean; optional)
+
+- isRight (boolean; optional)
+
+- items (list; optional)"""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'dash_mp_components'
+    _type = 'NotificationDropdown'
+    @_explicitize_args
+    def __init__(self, children=None, className=Component.UNDEFINED, items=Component.UNDEFINED, isRight=Component.UNDEFINED, isModal=Component.UNDEFINED, isHidden=Component.UNDEFINED, hasUnread=Component.UNDEFINED, id=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'id', 'className', 'hasUnread', 'isHidden', 'isModal', 'isRight', 'items']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['children', 'id', 'className', 'hasUnread', 'isHidden', 'isModal', 'isRight', 'items']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+        for k in []:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+        super(NotificationDropdown, self).__init__(children=children, **args)
---
+++ src/lib/components/navigation/NotificationDropdown.react.js
@@ -0,0 +1,21 @@
+import * as mpc from '@materialsproject/mp-react-components';
+import {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class NotificationDropdown extends Component {
+    render() {
+        return <mpc.NotificationDropdown {...this.props} />;
+    }
+}
+
+NotificationDropdown.propTypes = {
+    className: PropTypes.string,
+    items: PropTypes.array,
+    isRight: PropTypes.bool,
+    isModal: PropTypes.bool,
+    isHidden: PropTypes.bool,
+    hasUnread: PropTypes.bool,
+    children: PropTypes.node,
+    id: PropTypes.string,
+};
