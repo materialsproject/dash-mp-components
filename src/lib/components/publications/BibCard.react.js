790a4678ec2e97c609d71d2f37e0ce42b89024c3
yang-ruoxi <roxie@Live.cn>
2024-09-19 17:42:23 -0700

    add shortName prop to bibcard

--- src/lib/components/publications/BibCard.react.js
+++ src/lib/components/publications/BibCard.react.js
@@ -58,6 +58,10 @@ BibCard.propTypes = {
      * online publication, open access PDF, and bibtex
      */
     doi: PropTypes.string,
+    /**
+     * Shortened title of the article
+     */
+    shortName: PropTypes.string,
 
     /**
      * Set to true to prevent dynamically fetching a link to a free PDF of
